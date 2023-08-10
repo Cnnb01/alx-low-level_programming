06f83d088572438132b71bb092e21b9d66cc7771
Cnnb01 <nyamambanyaguch@gmail.com>
2023-08-10 12:02:03 -0700

    a C program

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,13 +1,12 @@
 #include <stdio.h>
-
+#include <unistd.h>
 /**
 *main - Entry point
 *
 *Return: Always 0 (Success)
 */
-int main (void)
+int main(void)
 {
-char mege[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-putchar(mege);
+fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
 return (1);
 }
