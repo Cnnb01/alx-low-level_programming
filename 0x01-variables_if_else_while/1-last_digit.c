d35bd26fd19c03676404b5e4ba623015c1e12741
Cnnb01 <nyamambanyaguch@gmail.com>
2023-08-11 13:43:33 -0700

    Checking the last digit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -19,13 +19,13 @@ if (lastdig > 5)
 {
 printf("Last digit of %d is %d and is greater than 5\n", n, lastdig);
 }
-else if (lastdig < 6)
+else if (lastdig == 0)
 {
-printf("Last digit of %d is %d and is less than 6 and not 0\n", n, lastdig);
+printf("Last digit of %d is %d and is 0\n", n, lastdig);
 }
 else
 {
-printf("Last digit of %d is %d and is 0\n", n, lastdig);
+printf("Last digit of %d is %d and is less than 6 and not 0\n", n, lastdig);
 }
 return (0);
 }
