95e0d6e7a93cae864c47381c694c5b8391ec19ba
Cnnb01 <nyamambanyaguch@gmail.com>
2023-08-13 07:13:24 -0700

    a program that prints all possible combinations of two two-digit numbers.

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main(void)
+{
+int n, n2;
+
+for (n = 0; n <= 99; n++)
+{
+for (n2 = n; n2 <= 99; n2++)
+{
+putchar(n / 10 + '0');
+putchar(n % 10 + '0');
+putchar(' ');
+putchar(n2 / 10 + '0');
+putchar(n2 % 10 + '0');
+if (n != 99 || n2 != 99)
+{
+putchar(',');
+putchar(' ');
+}
+}
+}
+putchar('\n');
+return 0;
+}
+
