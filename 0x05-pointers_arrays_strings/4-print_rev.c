e9d2c3b21aa098a45230ba0d190c60f24008ef76
Cnnb01 <nyamambanyaguch@gmail.com>
2023-08-21 07:36:02 -0700

    a function that prints a string, in reverse, followed by a new line.

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <string.h>
+/**/
+void print_rev(char *s)
+{
+int len, i;
+len = strlen(s);
+for (i = len - 1; i >= 0; i--)
+{
+_putchar(s[i]);
+}
+_putchar('\n');
+}
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,8 +1,10 @@
 #ifndef MOREF_FILE
 #define MOREF_FILE
+int _putchar(char c);
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
 int _strlen(char *s);
 void _puts(char *str);
+void print_rev(char *s);
 
 #endif
