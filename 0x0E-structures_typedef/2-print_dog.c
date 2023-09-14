2080293ac5a41f652baa926b68a2def985ec4b35
Cnnb01 <nyamambanyaguch@gmail.com>
2023-09-13 23:27:39 -0700

    a function that prints a struct dog

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -24,8 +24,8 @@ printf("Age: nil\n");
 }
 else
 {
-printf("Name:%s\n ", d->name);
-printf("Age:%.1f\n ", d->age);
-printf("Owner:%s\n", d->owner);
+printf("Name: %s\n", d->name);
+printf("Age: %.1f\n", d->age);
+printf("Owner: %s\n", d->owner);
 }
 }
