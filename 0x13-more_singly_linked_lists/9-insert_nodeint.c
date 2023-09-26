884d15158a96761454bcdef5fd2cf87f214e1a2b
Cnnb01 <nyamambanyaguch@gmail.com>
2023-09-26 22:23:40 +0300

    task9

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -1,42 +1,47 @@
 #include "lists.h"
+
 /**
-*insert_nodeint_at_index - a function that inserts a new
-*node at a given position.
-*@head: Pointer to the pointer of the head node.
-*@idx: the index
-*@n: the integer data
-*Return: Pointer to the new node or NULL on failure.
+* insert_nodeint_at_index - Inserts a new node at a
+*given position in a linked list.
+* @head: A pointer to the head of the linked list.
+* @idx: The index at which to insert the new node.
+* @n: The value to store in the new node.
 *
+* Return: If memory allocation fails or the index is invalid, return NULL.
+*         Otherwise, return a pointer to the new node.
 */
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
-	listint_t *newnode;
-	listint_t *temphead;
-	unsigned int count = 0;
+listint_t *new_node, *temp;
+unsigned int i;
 
-	if (*head == NULL)
-	{
-		return (NULL);
-	}
-	newnode = malloc(sizeof(listint_t));
-	if (newnode == NULL)
-	{
-		return (NULL);
-	}
-	temphead = *head;
-	while (temphead != NULL)
-	{
-		count++;
-		if (count != idx)
-		{
-			temphead = temphead->next;
-		}
-		else
-		{
-			newnode->next = temphead->next;
-			temphead->next = newnode;
-			newnode->n = n;
-		}
-	}
-	return (newnode);
+/* Allocate memory for the new node */
+new_node = malloc(sizeof(listint_t));
+if (new_node == NULL)
+return (NULL);
+/* Initialize the new node */
+new_node->n = n;
+if (idx == 0)
+{
+/* Insert at the beginning of the list */
+new_node->next = *head;
+*head = new_node;
+return (new_node);								        }
+
+temp = *head;
+for (i = 0; temp != NULL && i < idx - 1; i++)
+{
+/* Traverse the list to the position before the desired index */
+temp = temp->next;
 }
+if (temp == NULL)
+{
+/* Index is out of range; free the new node and return NULL */
+free(new_node);
+return (NULL);									    }
+/* Insert the new node into the list */
+new_node->next = temp->next;
+temp->next = new_node;
+return (new_node);
+}
+
