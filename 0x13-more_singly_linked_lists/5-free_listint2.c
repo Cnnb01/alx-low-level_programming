f2b1fa9369c515208f7fde86a84c8563e78a3fc0
Cnnb01 <nyamambanyaguch@gmail.com>
2023-09-26 12:28:24 +0300

    task5

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+/**
+*free_listint - a function that frees a listint_t lisi
+*@head: Pointer to the pointer of the head node.
+*/
+void free_listint2(listint_t **head)
+{
+listint_t *previous;
+listint_t *next;
+if (*head == NULL)
+{
+return;
+}
+previous = *head;
+while (previous != NULL)
+{
+next = previous->next;
+free(previous);
+previous = next;
+}
+*head = NULL;
+}
