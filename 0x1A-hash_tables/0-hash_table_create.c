6c98f4a2196dead6c930c680314ab89cdd920a74
Cnnb01 <nyamambanyaguch@gmail.com>
2024-01-03 17:50:18 +0300

    creates a hash table.

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -10,8 +10,9 @@ hash_table_t *hash_table_create(unsigned long int size)
 {
 	unsigned long int i;
 	hash_table_t *table = malloc(sizeof(hash_node_t));
+
 	table->size = size;
-	table->array = calloc(table->size,(sizeof(hash_node_t*)));
+	table->array = calloc (table->size, (sizeof(hash_node_t *)));
 	for(i = 0; i < table->size; i++)
 	{
 		table->array[i] = NULL;
