d7d58d23e7954836eb69cdb92e191d7ffafa3df0
Cnnb01 <nyamambanyaguch@gmail.com>
2024-05-08 08:56:25 +0300

    binary search

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -9,34 +9,32 @@
 
 int binary_search(int *array, size_t size, int value)
 {
-    size_t low;
-    size_t high;
-    size_t mid, i;
-    if (array == NULL || size == 0)
-    {
-        return (-1);
-    }
-    for (low = 0, high = size -1 ;low <= high;)
-    {   
-        printf("Searching in array: [%lu", low);
-        for (i = low + 1; i <= high; i++) {
-            printf(", %lu", i);
-        }
-        printf("]\n");
-        mid = (low + high)/2;
-        if (array[mid] == value)
-        {
-            return (mid);
-        }
-        else if (array[mid] < value)
-        {
-            low = mid + 1;
-        }
-        else
-        {
-            high = mid - 1;
-        }
-        
-    }
-    return -1;
+size_t low, high, mid, i;
+if (array == NULL || size == 0)
+{
+return (-1);
+}
+for (low = 0, high = size -1 ;low <= high;)
+{   
+printf("Searching in array: [%lu", low);
+for (i = low + 1; i <= high; i++) 
+{
+printf(", %lu", i);
+}
+printf("]\n");
+mid = (low + high)/2;
+if (array[mid] == value)
+{
+return (mid);
+}
+else if (array[mid] < value)
+{
+low = mid + 1;
+}
+else
+{
+high = mid - 1;
+}
+}
+return -1;
 }
